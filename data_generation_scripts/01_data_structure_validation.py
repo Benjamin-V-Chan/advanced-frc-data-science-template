7c4b650f8cbe02fc0456588fc2c61cb387bc5535
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-01-07 23:05:36 -0800

    01 data structure validation script JSON data loading & variables loading outline

--- data_generation_scripts/01_data_structure_validation.py
+++ data_generation_scripts/01_data_structure_validation.py
@@ -1,3 +1,4 @@
+import json
 from utility_functions.print_formats import seperation_bar
 
 # ===========================
@@ -11,6 +12,9 @@ expected_data_structure_path = 'expected_data_structure.json'
 # HELPER FUNCTIONS SECTION
 # ===========================
 
+def flatten_vars_in_dict(dictionary):
+    pass
+
 # ===========================
 # MAIN SCRIPT SECTION
 # ===========================
@@ -19,9 +23,21 @@ print(seperation_bar)
 print("Script 01: Data Structure Validation\n")
 
 # Get Data Generation Configuration JSON as Dict
+with open(data_generation_config_path) as json_file:
+    data_generation_config = json.load(json_file)
+print("\nData Generation Configuration:")
+print(json.dumps(data_generation_config, indent=4) + "\n")
 
 # Get Expected Data Structure JSON as Dict
+with open(expected_data_structure_path) as json_file:
+    expected_data_structure = json.load(json_file)
+print("\nExpected Data Structure:")
+print(json.dumps(expected_data_structure, indent=4) + "\n")
 
 # Get Expected Data Structure Variables
+expected_data_structure_vars = flatten_vars_in_dict(expected_data_structure["variables"])
+
+print()
+print(expected_data_structure_vars)
 
 print(seperation_bar)
