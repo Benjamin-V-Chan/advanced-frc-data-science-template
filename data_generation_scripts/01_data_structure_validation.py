7609ea2ee9a2d733318a5bd9f2cc9d69f0e128d2
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-01-07 23:02:52 -0800

    01 data structure validation script outline

---
+++ data_generation_scripts/01_data_structure_validation.py
@@ -0,0 +1,27 @@
+from utility_functions.print_formats import seperation_bar
+
+# ===========================
+# CONFIGURATION SECTION
+# ===========================
+
+data_generation_config_path = 'data_generation_config.json'
+expected_data_structure_path = 'expected_data_structure.json'
+
+# ===========================
+# HELPER FUNCTIONS SECTION
+# ===========================
+
+# ===========================
+# MAIN SCRIPT SECTION
+# ===========================
+
+print(seperation_bar)
+print("Script 01: Data Structure Validation\n")
+
+# Get Data Generation Configuration JSON as Dict
+
+# Get Expected Data Structure JSON as Dict
+
+# Get Expected Data Structure Variables
+
+print(seperation_bar)
