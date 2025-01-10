63059d983bdd6cf0c08cf24f3c1bf4f17eb93a0b
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-01-09 22:57:04 -0800

    updated helper functions

--- data_generation_scripts/01_data_structure_validation.py
+++ data_generation_scripts/01_data_structure_validation.py
@@ -20,13 +20,23 @@ def single_dict(dictionary):
             return False
     return True
 
-def flatten_vars_in_dict(dictionary, main_dict = {}):
+def flatten_vars_in_dict(dictionary, return_dict=None, prefix=""):
+    if return_dict is None:
+        return_dict = {}
+    
     for key in dictionary:
+        full_key = f"{prefix}.{key}" if prefix else key
         if single_dict(dictionary[key]):
-            main_dict[key] = dictionary[key]
+            # print(f"single_key added: {full_key}: {dictionary[key]}")
+            return_dict[full_key] = dictionary[key]
         else:
-            flatten_vars_in_dict(dictionary[key])
-    return main_dict
+            flatten_vars_in_dict(dictionary[key], return_dict, full_key)
+    
+    return return_dict
+
+def retrieve_json(json_path):
+    with open(json_path) as json_file:
+        return json.load(json_file)
 
 # ===========================
 # MAIN SCRIPT SECTION
