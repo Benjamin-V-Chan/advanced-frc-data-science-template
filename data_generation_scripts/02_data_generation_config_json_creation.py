4de2325d2ec40a2274cbf8e6bc7e560715cdc161
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-02-02 21:32:23 -0800

    02 data generation config json creation script coded outline/initialization

---
+++ data_generation_scripts/02_data_generation_config_json_creation.py
@@ -0,0 +1,42 @@
+# based off of info in expected_data_structure.json, will generate a new json that is a default template for data_generation_condig.json
+# should use info about var names and statistical data types to make sure template is good
+# users can further personalize the config later if needed
+
+import json
+from utils.seperation_bars import *
+from utils.dictionary_manipulation import *
+
+# ===========================
+# CONFIGURATION SECTION
+# ===========================
+
+data_generation_config_path = 'data_generation_config.json'
+expected_data_structure_path = 'expected_data_structure.json'
+
+# ===========================
+# CONSTANTS SECTION
+# ===========================
+
+statistical_data_type_options = ['quantitative', 'categorical', 'binary']
+valid_robot_positions = ['red_1', 'red_2', 'red_3', 'blue_1', 'blue_2', 'blue_3']
+
+# ===========================
+# MAIN SCRIPT SECTION
+# ===========================
+
+seperation_bar()
+print("Script 01: Data Generation Config JSON Creation\n")
+
+# Retrieve JSON Data
+small_seperation_bar("RETRIEVE expected_data_structure.json")
+
+# Retrieve Expected Data Structure JSON as Dict
+expected_data_structure = retrieve_json(expected_data_structure_path)
+print("\nExpected Data Structure JSON:")
+print(json.dumps(expected_data_structure, indent=4))
+
+
+
+# END OF SCRIPT
+
+seperation_bar()
