f463b6a797906f34217716d11d139235f28b52f0
BenjaminChan <benjamin.v.chan@gmail.com>
2025-02-23 23:36:32 -0800

    flatten_vars_in_dict function update in dictionary_manipulation local module

--- utils/dictionary_manipulation.py
+++ utils/dictionary_manipulation.py
@@ -4,6 +4,7 @@ def retrieve_json(json_path):
     with open(json_path) as json_file:
         return json.load(json_file)
 
+
 def single_dict(dictionary):
     if not isinstance(dictionary, dict):
         return False
@@ -13,14 +14,18 @@ def single_dict(dictionary):
     return True
 
 def flatten_vars_in_dict(dictionary, return_dict=None, prefix=""):
+    """Flattens only variable names but keeps their properties (statistical_data_type, values) intact."""
     if return_dict is None:
         return_dict = {}
-    
-    for key in dictionary:
+
+    for key, value in dictionary.items():
         full_key = f"{prefix}.{key}" if prefix else key
-        if single_dict(dictionary[key]):
-            return_dict[full_key] = dictionary[key]
+
+        if isinstance(value, dict) and "statistical_data_type" not in value:
+            # If the dictionary does NOT contain a 'statistical_data_type' key, keep flattening
+            flatten_vars_in_dict(value, return_dict, full_key)
         else:
-            flatten_vars_in_dict(dictionary[key], return_dict, full_key)
-    
+            # If we hit the final structure (statistical data type exists), store as-is
+            return_dict[full_key] = value
+
     return return_dict
