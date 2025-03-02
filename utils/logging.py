767d052901d6f88a17201fe7531e242708c25585
BenjaminChan <benjamin.v.chan@gmail.com>
2025-03-01 18:58:54 -0800

    logging module for utils

---
+++ utils/logging.py
@@ -0,0 +1,4 @@
+def log_message(level, message):
+    """Standardized logging format with timestamp."""
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"[{timestamp}] [{level}] {message}")
