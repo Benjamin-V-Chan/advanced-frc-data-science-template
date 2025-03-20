351be3ee9c30a811f283c96e44fb577d665a3a16
BenjaminChan <benjamin.v.chan@gmail.com>
2025-03-19 22:40:39 -0700

    02_seperate_jsons logging header text cleaner

--- data_analysis_preperation/02_separate_jsons.py
+++ data_analysis_preperation/02_separate_jsons.py
@@ -32,27 +32,27 @@ def main():
     # LOAD DATA
     log_header("Load Data")
 
-    log_info(f"Loading 'Raw Data' from '{RAW_DATA_PATH}'...")
+    log_info(f"Loading 'Raw Data' from '{RAW_DATA_PATH}'")
     raw_data = retrieve_json(RAW_DATA_PATH)
     
     
     # EXTRACT DATA
     log_header("Extract Data")
     
-    log_info(f"Extracting 'Matchapp Data' from 'Raw Data'...")
+    log_info(f"Extracting 'Matchapp Data' from 'Raw Data'")
     matchapp_data = raw_data["matchApp"]
 
-    log_info(f"Extracting 'Superapp Data' from 'Raw Data'...")
+    log_info(f"Extracting 'Superapp Data' from 'Raw Data'")
     superapp_data = raw_data["superApp"]
     
     
     # SAVE DATA
     log_header("Save Data")
     
-    log_info(f"Saving 'Matchapp Data' to '{MATCHAPP_DATA_PATH}'...")
+    log_info(f"Saving 'Matchapp Data' to '{MATCHAPP_DATA_PATH}'")
     save_json(MATCHAPP_DATA_PATH, matchapp_data)
     
-    log_info(f"Saving 'Superapp Data' to '{SUPERAPP_DATA_PATH}'...")
+    log_info(f"Saving 'Superapp Data' to '{SUPERAPP_DATA_PATH}'")
     save_json(SUPERAPP_DATA_PATH, superapp_data)
     
     
