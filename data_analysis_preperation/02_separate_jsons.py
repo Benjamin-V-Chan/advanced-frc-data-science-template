487af7b10101bbb732ae9736f0adb1602351d4a2
BenjaminChan <benjamin.v.chan@gmail.com>
2025-03-19 22:37:07 -0700

    02_seperate_jsons script complete. Data Extraction and Data saving sections succesfully implemented

--- data_analysis_preperation/02_separate_jsons.py
+++ data_analysis_preperation/02_separate_jsons.py
@@ -9,8 +9,8 @@ from utils.logging import *
 # ===========================
 
 RAW_DATA_PATH = "data/raw/raw_data.json"
-MATCHAPPS_DATA_PATH = "data/raw/raw_matchapps_data.json"
-SUPERAPPS_DATA_PATH = "data/raw/raw_superapps_data.json"
+MATCHAPP_DATA_PATH = "data/raw/raw_matchapp_data.json"
+SUPERAPP_DATA_PATH = "data/raw/raw_superapp_data.json"
 
 # ===========================
 # HELPER FUNCTIONS
@@ -24,15 +24,41 @@ SUPERAPPS_DATA_PATH = "data/raw/raw_superapps_data.json"
 
 def main():
     
+    # SCRIPT START
     script_start("[Data Analysis Preperation] 02 - Separate JSONs")
+    
+    
 
-    
-    
-    
+    # LOAD DATA
+    log_header("Load Data")
+
+    log_info(f"Loading 'Raw Data' from '{RAW_DATA_PATH}'...")
     raw_data = retrieve_json(RAW_DATA_PATH)
     
-    matchapps_data = raw_data["matchapps"]
-    superapps_data = raw_data["superapps"]
+    
+    # EXTRACT DATA
+    log_header("Extract Data")
+    
+    log_info(f"Extracting 'Matchapp Data' from 'Raw Data'...")
+    matchapp_data = raw_data["matchApp"]
+
+    log_info(f"Extracting 'Superapp Data' from 'Raw Data'...")
+    superapp_data = raw_data["superApp"]
+    
+    
+    # SAVE DATA
+    log_header("Save Data")
+    
+    log_info(f"Saving 'Matchapp Data' to '{MATCHAPP_DATA_PATH}'...")
+    save_json(MATCHAPP_DATA_PATH, matchapp_data)
+    
+    log_info(f"Saving 'Superapp Data' to '{SUPERAPP_DATA_PATH}'...")
+    save_json(SUPERAPP_DATA_PATH, superapp_data)
+    
+    
+    
+    # SCRIPT END
+    script_end("[Data Analysis Preperation] 02 - Separate JSONs")
 
 if __name__ == "__main__":
     main()
