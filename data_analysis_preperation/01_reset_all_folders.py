553e19dfc998c1e6ea61d4cc4018e01558e882d8
BenjaminChan <benjamin.v.chan@gmail.com>
2025-03-20 22:30:00 -0700

    added 'cleaned' folder for 01 data analysis prep script

--- data_analysis_preperation/01_reset_all_folders.py
+++ data_analysis_preperation/01_reset_all_folders.py
@@ -67,6 +67,7 @@ if __name__ == "__main__":
     
     config = {
         "data": {
+            "cleaned": {},
             "processed": {},  
             "raw": {
                 "raw_data.json": None
