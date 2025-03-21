da1aa59606440e19f6816cf386f523cbc54b018f
BenjaminChan <benjamin.v.chan@gmail.com>
2025-03-20 23:32:57 -0700

    03_data_cleaning script initial setup and file path config

---
+++ data_analysis_preperation/03_data_cleaning.py
@@ -0,0 +1,35 @@
+# CLEAN MATCHAPP AND SUPERAPP RAW DATA SEPERATLEY.
+
+from utils.seperation_bars import *
+from utils.dictionary_manipulation import *
+from utils.logging import *
+
+# ===========================
+# CONFIGURATION
+# ===========================
+
+RAW_MATCHAPP_DATA_PATH = "data/raw/raw_matchapp_data.json"
+RAW_SUPERAPP_DATA_PATH = "data/raw/raw_superapp_data.json"
+CLEANED_MATCHAPP_DATA_PATH = "data/cleaned/initial_cleaned_matchapp_data.json"
+CLEANED_SUPERAPP_DATA_PATH = "data/cleaned/initial_cleaned_superapp_data.json"
+EXPECTED_DATA_STRUCTURE = "config/expected_data_structure.json"
+
+# ===========================
+# HELPER FUNCTIONS
+# ===========================
+
+
+# ===========================
+# MAIN SCRIPT
+# ===========================
+
+
+def main():
+    
+    
+    
+    # SCRIPT END
+    script_end("[Data Analysis Preperation] 02 - Separate JSONs")
+
+if __name__ == "__main__":
+    main()
