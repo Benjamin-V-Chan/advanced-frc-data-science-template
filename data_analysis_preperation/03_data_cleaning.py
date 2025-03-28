2df04fdf20afb79b2c15804443f9a864f4f743bf
BenjaminChan <benjamin.v.chan@gmail.com>
2025-03-28 12:35:12 -0700

    03_data_cleaning main script coded outline and initialization

--- data_analysis_preperation/03_data_cleaning.py
+++ data_analysis_preperation/03_data_cleaning.py
@@ -25,10 +25,39 @@ EXPECTED_DATA_STRUCTURE = "config/expected_data_structure.json"
 
 def main():
     
+    # SCRIPT START
+    script_start("[Data Analysis Preperation] 03 - Data Cleaning")
+    
+    
+    
+    # LOAD DATA
+    log_header("Load Data")
+    
+    log_info(f"Loading 'Raw Matchapp Data' from '{RAW_MATCHAPP_DATA_PATH}'")
+    raw_matchapp_data = retrieve_json(RAW_MATCHAPP_DATA_PATH)
+    
+    log_info(f"Loading 'Raw Matchapp Data' from '{RAW_MATCHAPP_DATA_PATH}'")
+    raw_superapp_data = retrieve_json(RAW_SUPERAPP_DATA_PATH)
+    
+    
+    
+    # DATA CLEANING
+    log_header("Data Cleaning")
+    
+    
+    
+    # SAVE DATA
+    log_header("Save Data")
+    
+    log_info(f"Saving 'Cleaned Matchapp Data' to '{CLEANED_MATCHAPP_DATA_PATH}'")
+    save_json(CLEANED_MATCHAPP_DATA_PATH, cleaned_matchapp_data)
+    
+    log_info(f"Saving 'Cleaned Superapp Data' to '{CLEANED_SUPERAPP_DATA_PATH}'")
+    save_json(CLEANED_SUPERAPP_DATA_PATH, cleaned_superapp_data)
     
     
     # SCRIPT END
-    script_end("[Data Analysis Preperation] 02 - Separate JSONs")
+    script_end("[Data Analysis Preperation] 03 - Data Cleaning")
 
 if __name__ == "__main__":
     main()
