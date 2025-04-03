51bc16c5b171a732d36a11d782f0f25e6cb10aa0
BenjaminChan <benjamin.v.chan@gmail.com>
2025-04-02 23:18:27 -0700

    update for better naming convention

--- config_validation_scripts/01_expected_data_structure_data_validation.py
+++ config_validation_scripts/01_expected_data_structure_data_validation.py
@@ -239,12 +239,12 @@ def main():
     # LOAD CONFIG
     log_header("Load Config")
     
-    log_info(f"Loading 'Expected Data Structure JSON Config' from '{EXPECTED_DATA_STRUCTURE_PATH}'")
+    log_info(f"Loading 'Expected Data Structure Config' from '{EXPECTED_DATA_STRUCTURE_PATH}'")
     expected_data_structure = retrieve_json(EXPECTED_DATA_STRUCTURE_PATH)
 
-    log_info(f"Expected Data Structure JSON Config:\n{json.dumps(expected_data_structure, indent=4)}\n")
-
-
+    log_info(f"Expected Data Structure Config:\n{json.dumps(expected_data_structure, indent=4)}\n")
+    
+    
 
     # CONFIG VALIDATION
     log_header("Config Validation")
